3c7c104bed63a9e47e47b568a27919c0afdfe871
Vera Gonzalez <me@vera.lgbt>
2025-02-06 23:11:46 -0500

    more rewriting of enums

--- src/polyhedron/shape/planarity/state.rs
+++ src/polyhedron/shape/planarity/state.rs
@@ -26,18 +26,18 @@ pub type Time = usize;
 
 #[derive(Copy, Clone, Debug)]
 pub enum DfsEvent {
-    Discover(VertexId, Time),
+    Discover(VertexId),
     /// An edge of the tree formed by the traversal.
-    TreeEdge(VertexId, VertexId),
+    TreeEdge(Edge),
     /// An edge to an already visited node.
-    BackEdge(VertexId, VertexId),
+    BackEdge(Edge),
     /// A cross or forward edge.
     ///
     /// For an edge *(u, v)*, if the discover time of *v* is greater than *u*,
     /// then it is a forward edge, else a cross edge.
-    CrossForwardEdge(VertexId, VertexId),
+    CrossForwardEdge(Edge),
     /// All edges from a node have been reported.
-    Finish(VertexId, Time),
+    Finish(VertexId),
 }
 
 /// Similar to ``DfsEvent`` plus an extra event ``FinishEdge``
@@ -72,38 +72,37 @@ impl LRState {
 
     pub fn lr_orientation_visitor(&mut self, event: DfsEvent) {
         match event {
-            DfsEvent::Discover(v, _) => {
+            DfsEvent::Discover(v) => {
                 if let Entry::Vacant(entry) = self.height.entry(v) {
                     entry.insert(0);
                     self.roots.push(v);
                 }
             }
-            DfsEvent::TreeEdge(v, w) => {
-                let ei = [v, w].into();
+            DfsEvent::TreeEdge(edge) => {
+                let [v, w] = edge.inner();
                 let v_height = self.height[&v];
                 let w_height = v_height + 1;
 
-                self.edge_parent.insert(w, ei);
+                self.edge_parent.insert(w, edge);
                 self.height.insert(w, w_height);
                 // now initialize low points.
-                self.low_point.insert(ei, v_height);
-                self.low_point_2.insert(ei, w_height);
+                self.low_point.insert(edge, v_height);
+                self.low_point_2.insert(edge, w_height);
             }
-            DfsEvent::BackEdge(v, w) => {
+            DfsEvent::BackEdge(edge) => {
                 // do *not* consider ``(v, w)`` as a back edge if ``(w, v)`` is a tree edge.
-                let edge = [v, w].into();
+                let [v, w] = edge.inner();
                 if Some(&edge) != self.edge_parent.get(&v) {
                     self.low_point.insert(edge, self.height[&w]);
                     self.low_point_2.insert(edge, self.height[&v]);
                 }
             }
-            DfsEvent::Finish(v, _) => {
+            DfsEvent::Finish(v) => {
                 for w in self.graph.neighbors(v) {
-                    //let ei = (v, w);
-                    let ei: Edge = [v, w].into();
+                    let edge: Edge = [v, w].into();
 
                     // determine nesting depth.
-                    let low = match self.low_point.get(&ei) {
+                    let low = match self.low_point.get(&edge) {
                         Some(val) => *val,
                         None =>
                         // if ``lowpt`` does *not* contain edge ``(v, w)``, it means
@@ -114,28 +113,28 @@ impl LRState {
                         }
                     };
 
-                    if self.low_point_2[&ei] < self.height[&v] {
+                    if self.low_point_2[&edge] < self.height[&v] {
                         // if it's chordal, add one.
-                        self.nesting_depth.insert(ei, 2 * low + 1);
+                        self.nesting_depth.insert(edge, 2 * low + 1);
                     } else {
-                        self.nesting_depth.insert(ei, 2 * low);
+                        self.nesting_depth.insert(edge, 2 * low);
                     }
 
                     // update lowpoints of parent edge.
                     if let Some(e_par) = self.edge_parent.get(&v) {
-                        match self.low_point[&ei].cmp(&self.low_point[e_par]) {
+                        match self.low_point[&edge].cmp(&self.low_point[e_par]) {
                             Ordering::Less => {
                                 self.low_point_2.insert(
                                     *e_par,
-                                    self.low_point[e_par].min(self.low_point_2[&ei]),
+                                    self.low_point[e_par].min(self.low_point_2[&edge]),
                                 );
-                                self.low_point.insert(*e_par, self.low_point[&ei]);
+                                self.low_point.insert(*e_par, self.low_point[&edge]);
                             }
                             Ordering::Greater => {
-                                modify_if_min(&mut self.low_point_2, *e_par, self.low_point[&ei]);
+                                modify_if_min(&mut self.low_point_2, *e_par, self.low_point[&edge]);
                             }
                             _ => {
-                                let val = self.low_point_2[&ei];
+                                let val = self.low_point_2[&edge];
                                 modify_if_min(&mut self.low_point_2, *e_par, val);
                             }
                         }
@@ -148,14 +147,12 @@ impl LRState {
 
     pub fn lr_testing_visitor(&mut self, event: LRTestDfsEvent) -> Result<(), NonPlanar> {
         match event {
-            LRTestDfsEvent::TreeEdge(v, w) => {
-                let edge: Edge = [v, w].into();
+            LRTestDfsEvent::TreeEdge(edge) => {
                 if let Some(&last) = self.stack.last() {
                     self.stack_emarker.insert(edge, last);
                 }
             }
-            LRTestDfsEvent::BackEdge(v, w) => {
-                let edge: Edge = [v, w].into();
+            LRTestDfsEvent::BackEdge(edge) => {
                 if let Some(&last) = self.stack.last() {
                     self.stack_emarker.insert(edge, last);
                 }
@@ -163,8 +160,8 @@ impl LRState {
                 let c_pair = ConflictPair::new(Interval::default(), Interval::new(edge, edge));
                 self.stack.push(c_pair);
             }
-            LRTestDfsEvent::FinishEdge(v, w) => {
-                let edge: Edge = [v, w].into();
+            LRTestDfsEvent::FinishEdge(edge) => {
+                let [v, w] = edge.inner();
                 if self.low_point[&edge] < self.height[&v] {
                     // ei has return edge
                     let e_par = self.edge_parent[&v];
@@ -181,13 +178,12 @@ impl LRState {
                 }
             }
             LRTestDfsEvent::Finish(v) => {
-                if let Some(&e) = self.edge_parent.get(&v) {
-                    //(*e)
-                    let u = e.0;
+                if let Some(&edge) = self.edge_parent.get(&v) {
+                    let u = edge.v();
                     self.remove_back_edges(u);
 
                     // side of ``e = (u, v)` is side of a highest return edge
-                    if self.low_point[&e] < self.height[&u] {
+                    if self.low_point[&edge] < self.height[&u] {
                         if let Some(top) = self.stack.last() {
                             let e_high = match (top.left.high(), top.right.high()) {
                                 (Some(hl), Some(hr)) => {
@@ -205,7 +201,7 @@ impl LRState {
                                     unreachable!()
                                 }
                             };
-                            self.eref.insert(e, *e_high);
+                            self.eref.insert(edge, *e_high);
                         }
                     }
                 }
